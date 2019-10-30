adc68bc5b972387f6e0b45e9265cfb98cb645667
Sagalpreet Singh <52735377+sagalpreet@users.noreply.github.com>
2019-10-30 23:27:03 +0530

    Update creating_users.py

--- creating_users.py
+++ creating_users.py
@@ -1,1 +1,16 @@
 #this file is meant to create user files from source file downloaded from the snap stanford.
+f=open('facebook_combined.txt','r',encoding='utf-8')
+s=' '
+while s!='':
+  s=f.readline()
+  if s=='':
+    break
+  l=s.split()
+  l[0]=l[0]+'.txt'
+  l[1]=l[1]+'.txt'
+  g=open(l[0],'a+',encoding='utf-8')
+  g.write(l[1]+'\n')
+  g.close()
+  g=open(l[1],'a+',encoding='utf-8')
+  g.write(l[0]+'\n')
+  g.close()
