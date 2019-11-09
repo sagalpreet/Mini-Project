4e2df3c535100ec59edc284aff64ef344159cd9a
nitish283 <57191098+nitish283@users.noreply.github.com>
2019-11-09 20:16:21 +0530

    Add files via upload

---
+++ Score&friendUpdate.py
@@ -0,0 +1,43 @@
+friend={}
+
+def comp(a,b):
+    f1=open(a+'.txt','r')
+    f2=open(b+'.txt','r')
+    s1=((f1.readlines()).split())[4:]
+    s2=((f2.readlines()).split())[4:]
+    k=0
+    if s1[0]==s2[0] or s1[1]==s2[1]:
+        k+=1
+    if s1[2]==s2[2]:
+        k+=2
+    if s1[2]==s2[2]:
+        k-=1
+    f1.close()
+    f2.close()
+    f1=open(a+'.txt','r')
+    f2=open(b+'.txt','r')
+    s1=set(((f1.readlines()).split())[4:])
+    s2=set((f2.readlines()).split()[4:])
+    s=s1&s2
+    l=3*len(s)
+    marks=k+l
+    friend[b]=marks
+
+    
+base=[]
+a=input()
+f=open(a+'.txt')
+original=((f.readlines()).split())[4:]
+for i in range(4806):
+    if(str(i)+'.txt' in original or str(i)==a):
+       continue
+    else:
+        base.append(str(i))
+
+for i in base:
+    comp(a,i)
+
+marklist=list(friend.values())
+for i in range(len(friend)):
+    print(friend[marklist.index(max(marklist))])
+    marklist.remove(max(marklist))
