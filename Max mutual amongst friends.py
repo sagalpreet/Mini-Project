d796263aa580c9655a73d397365d6aa0b152fd1c
nitish283 <57191098+nitish283@users.noreply.github.com>
2019-11-11 19:53:31 +0530

    Add files via upload
    EDIT

--- Max mutual amongst friends.py
+++ Max mutual amongst friends.py
@@ -1,6 +1,7 @@
 def bestmutual(org,l):   # l is list of original friends, org is person for whom we are finding
     d={}
     c=0
+    ll=set(l)
     for i range(len(l)):
         f=open(l[i])
         s=f.readlines()
@@ -9,14 +10,22 @@ def bestmutual(org,l):   # l is list of original friends, org is person for whom
             if(x==org or (x in d) or (x in l)):
                 continue
             else:
-                for k in range(len(l)):  # checking how many friends have x in common
-                    if(i!=k):
-                        ff=open(l[i])
-                        if(ff.find(x)==1):
-                            c+=1
+                ff=open(x)
+                fr=set(ff.readlines().split()[4:])
+                c=fr.intersection(ll)
                 d.update({x:c})
-                c=0
+
+                
+
     v=list(d.values()) # taking values from dictionary
     for i in range(10): # top 10
         print(d[v.index(max(v))])
         v.remove(max(v))
+
+    #            for k in range(len(l)):  # checking how many friends have x in common
+     #               if(i!=k):
+      #                  ff=open(l[i])
+       #                 if(ff.find(x)==1):
+        #                    c+=1
+         #       d.update({x:c})
+          #      c=0
