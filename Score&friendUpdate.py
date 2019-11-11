856fec37a487e792b540f98504b3af3b043d2241
Sagalpreet Singh <52735377+sagalpreet@users.noreply.github.com>
2019-11-11 19:24:18 +0530

    Update Score&friendUpdate.py

--- Score&friendUpdate.py
+++ Score&friendUpdate.py
@@ -3,8 +3,8 @@ friend={}
 def comp(a,b):
     f1=open(a+'.txt','r')
     f2=open(b+'.txt','r')
-    s1=((f1.readlines()).split())[4:]
-    s2=((f2.readlines()).split())[4:]
+    s1=((f1.readlines()))
+    s2=((f2.readlines()))
     k=0
     if s1[0]==s2[0] or s1[1]==s2[1]:
         k+=1
@@ -16,8 +16,8 @@ def comp(a,b):
     f2.close()
     f1=open(a+'.txt','r')
     f2=open(b+'.txt','r')
-    s1=set(((f1.readlines()).split())[4:])
-    s2=set((f2.readlines()).split()[4:])
+    s1=set(((f1.readlines()))[4:])
+    s2=set((f2.readlines())[4:])
     s=s1&s2
     l=3*len(s)
     marks=k+l
@@ -27,8 +27,8 @@ def comp(a,b):
 base=[]
 a=input()
 f=open(a+'.txt')
-original=((f.readlines()).split())[4:]
-for i in range(4806):
+original=((f.readlines()))[4:]
+for i in range(4039):
     if(str(i)+'.txt' in original or str(i)==a):
        continue
     else:
@@ -39,7 +39,7 @@ for i in base:
 
 marklist=list(friend.values())
 keylist=list(friend.keys())
-for i in range(marklist):
+for i in range(len(marklist)):
     print(keylist[marklist.index(max(marklist))])
     keylist.remove(marklist.index(max(marklist)))
     marklist.remove(max(marklist))
