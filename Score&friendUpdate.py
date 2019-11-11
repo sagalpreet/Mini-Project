f920240ba1009f4e011cbd0b34dfb9e703815738
Sagalpreet Singh <52735377+sagalpreet@users.noreply.github.com>
2019-11-12 00:48:34 +0530

    Update Score&friendUpdate.py

--- Score&friendUpdate.py
+++ Score&friendUpdate.py
@@ -1,8 +1,22 @@
 friend={}
 
+def arrange(l,d):
+    if len(l)>0:
+        k=int(d[l[-1]][0])
+        a=[]
+        b=[]
+        for i in range(len(l)-1):
+            if int(d[l[i]][0])<=k:
+                a.append(l[i])
+            elif int(d[l[i]][0])>=k:
+                b.append(l[i])
+        return arrange(a,d)+[l[-1]]+arrange(b,d)
+    else:
+        return []
+
 def comp(a,b):
-    f1=open(a+'.txt','r')
-    f2=open(b+'.txt','r')
+    f1=open(a,'r')
+    f2=open(b,'r')
     s1=((f1.readlines()))
     s2=((f2.readlines()))
     k=0
@@ -14,33 +28,36 @@ def comp(a,b):
         k-=1
     f1.close()
     f2.close()
-    f1=open(a+'.txt','r')
-    f2=open(b+'.txt','r')
+    f1=open(a,'r')
+    f2=open(b,'r')
     s1=set(((f1.readlines()))[4:])
     s2=set((f2.readlines())[4:])
     s=s1&s2
+    f1.close()
+    f2.close()
     l=3*len(s)
     marks=k+l
-    friend[b]=marks
+    friend[b]=[marks,len(s)]
 
-    
-base=[]
 a=input()
 f=open(a+'.txt')
 original=((f.readlines()))[4:]
-for i in range(4039):
-    if(str(i)+'.txt' in original or str(i)==a):
-       continue
-    else:
-        base.append(str(i))
-
-for i in base:
-    comp(a,i)
-
-marklist=list(friend.values())
-keylist=list(friend.keys())
-for i in range(len(marklist)):
-    print(keylist[marklist.index(max(marklist))])
-    keylist.remove(marklist.index(max(marklist)))
-    marklist.remove(max(marklist))
+for i in original:
+    g=open(i[:-1],'r')
+    original_next=g.readlines()[4:]
+    for j in original_next:
+        if j not in original and j!=a+'.txt\n':
+            comp(a+'.txt',j[:-1])
+    g.close()
 f.close()
+#print(friend)
+all=[]
+#sort
+for i in friend:
+    all.append(i)
+all=arrange(all,friend)
+for i in range(-1,-11,-1):
+    try:
+        print('You have',friend[(all[i])][1],'mutual friends with',all[i])   
+    except:
+        break
